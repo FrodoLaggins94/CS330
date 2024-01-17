586c2384fc08ea4b811bc979ab55cc0837e886c3
FrodoLaggins94 <150277455+FrodoLaggins94@users.noreply.github.com>
2024-01-17 22:05:32 +0000

    created header file.

---
+++ Assignments/asgn2.h
@@ -0,0 +1,7 @@
+#ifndef __asgn2__
+#define __asgn2__
+/* the two lines above check to ensure
+we haven't already included this header*/
+/* your functions go here */
+// Note: main() goes in the asgn2.c file
+#endif
