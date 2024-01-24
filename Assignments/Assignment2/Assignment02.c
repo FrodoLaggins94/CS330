922d1a5460d78b5eb3f8b125ee22fbaa6625e876
FrodoLaggins94 <150277455+FrodoLaggins94@users.noreply.github.com>
2024-01-24 18:30:11 +0000

    two functions

---
+++ Assignments/Assignment2/Assignment02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+   cubeOfOdd(10);
+   cubeOfOdd(17);
+   cubeOfOdd(4);
+   
+   introToCS330(6);
+   introToCS330(35);
+   introToCS330(21);
+   introToCS330(5);
+   introToCS330(6);
+}
